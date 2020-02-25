60420befe68d11c8f1ad812026aee63e81342b70
vveennyy <venybylaev@gmail.com>
2020-02-25 03:42:20 +0300

    2

---
+++ vveennyy/vveennyy/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using InputAccuracy;
+
+namespace vveennyy
+{
+    class Account
+    {
+        private string _Nomination { get; set; }
+        private int _AccountNumber { get; set; }
+        private double _Amount { get; set; }
+        public Account()
+        {
+            this._Nomination = "No name";
+            this._AccountNumber = 0000;
+            this._Amount = 0.0f;
+        }
+        public Account(string _nomination, int _accountNumber, double _amount)
+        {
+            this._Nomination = _nomination;
+            this._AccountNumber = _accountNumber;
+            this._Amount = _amount;
+        }
+        public string NominationEdit(string _nomination)
+        {
+            this._Nomination = _nomination;
+            return _nomination;
+        }
+        public int AccountNumberEdit(int _accountNumber)
+        {
+            this._AccountNumber = _accountNumber;
+            return _accountNumber;
+        }
+        public double AmountEdit(double _amount)
+        {
+            this._Amount = _amount;
+            return _amount;
+        }
+        public string NominationInfo()
+        {
+
+            return this._Nomination;
+        }
+        public int AccountNumberInfo()
+        {
+
+            return this._AccountNumber;
+        }
+        public double AmountInfo()
+        {
+
+            return this._Amount;
+        }
+        public override string ToString()
+        {
+            return string.Format("наименование:{0}\nНомер счета:{1}\nСумму:{2}", this._Nomination, this._AccountNumber, this._Amount);
+        }
+        public void DataInput()
+        {
+            Class1 input = new Class1();
+            Console.WriteLine("введите наименование");
+            NominationEdit(input._stringName(Console.ReadLine()));
+            Console.WriteLine("введите номер счета");
+            AccountNumberEdit(input._int(Convert.ToInt32(Console.ReadLine())));
+            Console.WriteLine("введите сумму");
+            AmountEdit(input._double(Convert.ToDouble(Console.ReadLine())));
+        }
+    }
+}
